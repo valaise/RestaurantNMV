3f45b327bc272add98b130b4b97edac9d88fc77b
mistral valaise <nvalaise@outlook.de>
2021-12-06 08:42:15 +0100

    DishController

---
+++ Controllers/DishesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantNMV.Models;
+
+namespace RestaurantNMV.Controllers
+{
+    public class DishesController
+    {
+        public class DishesController : Controller
+        {
+            [Route("api/[controller]")]
+            public class DishesController : Controller
+            {
+                [HttpGet]
+                public IEnumerable<Dish> Get()
+                {
+                    return new[]
+{
+                        new Dish
+                        {
+                            Name ="Erste Name",
+                            Description = "meine test zum Bescchreiben",
+                            Id = 1,
+                            Price = 3.99
+                        }
+                    };
+}
+            }
+        }
+    }
+}
